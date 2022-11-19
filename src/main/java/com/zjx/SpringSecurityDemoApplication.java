d6180d6da3a227b0276faa4e6dc085f62382192c
476554858@qq.com <476554858@qq.com>
2022-11-19 16:29:49 +0800

    test

--- src/main/java/com/zjx/SpringSecurityDemoApplication.java
+++ src/main/java/com/zjx/SpringSecurityDemoApplication.java
@@ -8,9 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 @SpringBootApplication
 public class SpringSecurityDemoApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityDemoApplication.class, args);
 	}
-
 }
